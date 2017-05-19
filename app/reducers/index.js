f2551c1c631e2e3e07b4fda617a5ee78aafcbc25
Jonathan Maddison <jonathanwmaddison@gmail.com>
2017-05-19 10:14:01 -0700

    add comments to reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,13 +3,55 @@ import ImageLibraryReducer from './ImageLibraryReducer'
 import {
     SEARCH_FLICKR, REQUEST_PHOTOS, RECEIVE_PHOTOS
 } from '../actions'
-
-function selectedTag(state = 'San Francisco', action) {
+/**
+ * Sets currentSearch state. initializes with San Francisco
+*/
+function currentSearch(state = 'San Francisco', action) {
     switch(action.type) {
-        case 
+        case SEARCH_FLICKR:
+            return action.searchTerm
+        default:
+            return state
     }
 }
-
+/**
+ * Sets state of photos(photos to be display) for the current search term and sets isfetching.
+*/
+function photos(state = {
+    isFetching: false,
+    items: []
+}, action) {
+    switch(action.type) {
+        case REQUEST_PHOTOS:
+            return Object.assign({}, state, {
+                isFetching: true,
+            })
+        case RECEIVE_PHOTOS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                items: action.photos,
+                lastUpdated: action.received
+            })
+         default:
+            return state
+    }
+}
+/**
+ * Keeps track of photos the user has previously searched.
+*/
+function photosBySearch(state = {}, action) {
+    switch(action.type) {
+        case RECEIVE_PHOTOS:
+        case REQUEST_PHOTOS:
+            return Object.assign({}, state, {
+                [action.searchTerm]: photos(state[action.searchTerm], action)
+            })
+        default:
+            return state
+    }
+}
 export default combineReducers({
-    imageLibrary: ImageLibraryReducer 
+    imageLibrary: ImageLibraryReducer,
+    photosBySearch,
+    currentSearch
 })
