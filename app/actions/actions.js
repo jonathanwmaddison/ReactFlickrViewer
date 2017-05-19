d4529973c25dade8e84e67d684c383b121a0383e
Jonathan Maddison <jonathanwmaddison@gmail.com>
2017-05-18 17:36:35 -0700

    add actions for api call

---
+++ app/actions/actions.js
@@ -0,0 +1,28 @@
+export const SEARCH_FLICKR = 'SEARCH_FLICKR';
+
+export function searchFlickr(searchTerm) {
+    return {
+        type: SEARCH_FLICKR,
+        searchTerm
+    }
+}
+
+export const REQUEST_PHOTOS = 'REQUEST_PHOTOS';
+
+function requestPhotos(searchTerm) {
+    return {
+        type: REQUEST_PHOTOS,
+        searchTerm
+    }
+}
+
+export const RECEIVE_PHOTOS = 'RECEIVE_PHOTOS';
+
+function recievePhotos(searchTerm, json) {
+    return {
+        type: RECEIVE_PHOTOS,
+        searchTerm,
+        photos: json.items,
+        recieved: Date.now()
+    }
+}
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,5 +1,14 @@
 import { combineReducers } from 'redux';
 import ImageLibraryReducer from './ImageLibraryReducer'
+import {
+    SEARCH_FLICKR, REQUEST_PHOTOS, RECEIVE_PHOTOS
+} from '../actions'
+
+function selectedTag(state = 'San Francisco', action) {
+    switch(action.type) {
+        case 
+    }
+}
 
 export default combineReducers({
     imageLibrary: ImageLibraryReducer 
